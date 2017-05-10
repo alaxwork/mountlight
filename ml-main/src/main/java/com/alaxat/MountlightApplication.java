55914937c56cc6e25eef29d04c1126ddc6d1c56f
Alexander Schmidt <work@alaxat.com>
2017-05-10 15:35:17 +0200

    init commit

---
+++ ml-main/src/main/java/com/alaxat/MountlightApplication.java
@@ -0,0 +1,12 @@
+package com.alaxat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountlightApplication.class, args);
+	}
+}
