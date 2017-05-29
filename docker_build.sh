45d41014cf09e455fba4ec18fd9e0f3d8f26c240
Alexander Schmidt <work@alaxat.com>
2017-05-29 08:41:00 +0200

    refactor gradle

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./gradlew clean build
-docker build -t alaxat/ml-discovery ./ml-discovery
-docker build -t alaxat/ml-order ./ml-order
-docker build -t alaxat/ml-service ./ml-service
-docker build -t alaxat/ml-zipkin ./ml-zipkin
+docker build -t alaxat/ml-discovery     ./ml-discovery
+docker build -t alaxat/ml-order         ./ml-order
+docker build -t alaxat/ml-service       ./ml-service
+docker build -t alaxat/ml-zipkin        ./ml-zipkin
