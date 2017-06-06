4302da8fecdfb9ea0f34cf15ab93c445afc44ca3
Alexander Schmidt <work@alaxat.com>
2017-06-06 08:23:36 +0200

    adjust logback config in order project

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ./gradlew clean build
+docker rmi alaxat/ml-zipkin alaxat/ml-customer alaxat/ml-order alaxat/ml-discovery
 docker build -t alaxat/ml-discovery     ./ml-discovery
 docker build -t alaxat/ml-order         ./ml-order
 docker build -t alaxat/ml-customer       ./ml-customer
--- ml-order/src/main/java/com/alaxat/mountlight/order/rest/OrderController.java
+++ ml-order/src/main/java/com/alaxat/mountlight/order/rest/OrderController.java
@@ -2,22 +2,25 @@ package com.alaxat.mountlight.order.rest;
 
 
 import com.alaxat.mountlight.order.dto.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
 public class OrderController {
 
-    private Logger logger = Logger.getLogger(OrderController.class.getName());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     @RequestMapping("/orders/customer/{customer}")
     List<Order> findByCustomer(@PathVariable("customer") Integer customerId) {
-        logger.info("Geting orders by customer id" + customerId);
+        logger.info("Geting orders by customer id {}", customerId);
+        logger.info("{} test", "+3");
         return Collections.singletonList(new Order().setName("Android phone"));
     }
 }
